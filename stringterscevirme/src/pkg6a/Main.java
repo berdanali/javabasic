d7740aaa511736700dd407a8db24b65239d3069e
lerraytss <101890381+lerraytss@users.noreply.github.com>
2022-10-12 18:07:06 +0300

    Add files via upload
    kendısıne parametre olarak gelen tek boyutlu strıng dızısındekı elemanları kendı ıcınde ters cevırıp gerı donduren method

---
+++ stringterscevirme/src/pkg6a/Main.java
@@ -0,0 +1,28 @@
+package pkg6a;
+import java.util.Arrays;
+public class Main {
+ public static String[] tersCevir(String[] a){
+        String[] str1 = new String[a.length];
+        String gecici = "";
+        for(int i = 0; i < a.length; i++){
+            for(int k = a[i].length()-1; k >= 0 ; k--){
+                gecici += a[i].charAt(k);
+            }
+            str1[i] = gecici;
+            gecici = "";
+        }
+        return str1;
+    } 
+    public static void main(String[] args) {
+       String[] str = {"Mert", "Ozan", "Fatih"};
+        System.out.println(Arrays.toString(tersCevir(str)));
+    }
+
+
+  
+}
+
+
+
+    
+
